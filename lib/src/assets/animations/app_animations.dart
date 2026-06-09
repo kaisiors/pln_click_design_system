7c0cdff70c60a63e9069a8452f15d7d7f9fca9aa
sahrulramdani <sahrul.ramdani@iconpln.co.id>
2026-06-09 14:30:01 +0700

    style: add icon from pln click

--- lib/src/assets/animations/app_animations.dart
+++ lib/src/assets/animations/app_animations.dart
@@ -3,12 +3,12 @@ abstract class AppAnimations {
   AppAnimations._();
 
   // Group: GIF
-  static const String animationOnBoarding1 = 'assets/animations/gif/animation-on-boarding-1.gif';
+  static const String animationBiometrics = 'assets/animations/gif/animation-biometrics.gif';
   static const String animationOnBoarding2 = 'assets/animations/gif/animation-on-boarding-2.gif';
-  static const String animationOnBoarding3 = 'assets/animations/gif/animation-on-boarding-3.gif';
 
   // Group: LOTTIE
-  static const String animationCharging = 'assets/animations/lottie/animation-charging.json';
   static const String animationLoadingPrimary = 'assets/animations/lottie/animation-loading-primary.json';
   static const String animationLoadingWhite = 'assets/animations/lottie/animation-loading-white.json';
+  static const String animationLottieBiometrics2 = 'assets/animations/lottie/animation-lottie-biometrics-2.json';
+  static const String animationLottieBiometrics = 'assets/animations/lottie/animation-lottie-biometrics.json';
 }
