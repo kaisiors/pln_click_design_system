e3b48ff93914931e48ce039a63ef78d03757daac
Muhammad Kautsar <kautsarmuhammad19@gmail.com>
2026-06-09 11:36:47 +0700

    fix: theme & text style

--- lib/src/typography/app_text_styles.dart
+++ lib/src/typography/app_text_styles.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pln_click_design_system/pln_click_design_system.dart';
 
-import 'app_line_height.dart';
-
 abstract class AppTextStyles {
   AppTextStyles._();
 
@@ -14,39 +12,38 @@ abstract class AppTextStyles {
 
   static final TextStyle _textStyle = TextStyle(package: _package, fontFamily: AppFonts.primary);
 
-  static TextStyle headingLarge = _textStyle.copyWith(fontSize: AppFontSize.xxl, fontWeight: AppFontWeight.semibold, height: _height(AppLineHeight.xl, AppFontSize.xxl));
+  static TextStyle f10w400 = _textStyle.copyWith(fontSize: AppFontSize.xxs, fontWeight: AppFontWeight.regular);
+  static TextStyle f10w500 = _textStyle.copyWith(fontSize: AppFontSize.xxs, fontWeight: AppFontWeight.medium);
+  static TextStyle f10w600 = _textStyle.copyWith(fontSize: AppFontSize.xxs, fontWeight: AppFontWeight.semibold);
+  static TextStyle f10w700 = _textStyle.copyWith(fontSize: AppFontSize.xxs, fontWeight: AppFontWeight.bold);
 
-  static TextStyle headingMedium = _textStyle.copyWith(fontSize: AppFontSize.xl, fontWeight: AppFontWeight.semibold, height: _height(AppLineHeight.lg, AppFontSize.xl));
+  static TextStyle f12w400 = _textStyle.copyWith(fontSize: AppFontSize.xs, fontWeight: AppFontWeight.regular);
+  static TextStyle f12w500 = _textStyle.copyWith(fontSize: AppFontSize.xs, fontWeight: AppFontWeight.medium);
+  static TextStyle f12w600 = _textStyle.copyWith(fontSize: AppFontSize.xs, fontWeight: AppFontWeight.semibold);
+  static TextStyle f12w700 = _textStyle.copyWith(fontSize: AppFontSize.xs, fontWeight: AppFontWeight.bold);
 
-  static TextStyle headingSmall = _textStyle.copyWith(fontSize: AppFontSize.lg, fontWeight: AppFontWeight.semibold, height: _height(AppLineHeight.lg, AppFontSize.lg));
+  static TextStyle f14w400 = _textStyle.copyWith(fontSize: AppFontSize.sm, fontWeight: AppFontWeight.regular);
+  static TextStyle f14w500 = _textStyle.copyWith(fontSize: AppFontSize.sm, fontWeight: AppFontWeight.medium);
+  static TextStyle f14w600 = _textStyle.copyWith(fontSize: AppFontSize.sm, fontWeight: AppFontWeight.semibold);
+  static TextStyle f14w700 = _textStyle.copyWith(fontSize: AppFontSize.sm, fontWeight: AppFontWeight.bold);
 
-  static TextStyle titleLarge = _textStyle.copyWith(fontSize: AppFontSize.md, fontWeight: AppFontWeight.semibold, height: _height(AppLineHeight.md, AppFontSize.md));
+  static TextStyle f16w400 = _textStyle.copyWith(fontSize: AppFontSize.md, fontWeight: AppFontWeight.regular);
+  static TextStyle f16w500 = _textStyle.copyWith(fontSize: AppFontSize.md, fontWeight: AppFontWeight.medium);
+  static TextStyle f16w600 = _textStyle.copyWith(fontSize: AppFontSize.md, fontWeight: AppFontWeight.semibold);
+  static TextStyle f16w700 = _textStyle.copyWith(fontSize: AppFontSize.md, fontWeight: AppFontWeight.bold);
 
-  static TextStyle titleMedium = _textStyle.copyWith(fontSize: AppFontSize.sm, fontWeight: AppFontWeight.semibold, height: _height(AppLineHeight.sm, AppFontSize.sm));
+  static TextStyle f18w400 = _textStyle.copyWith(fontSize: AppFontSize.lg, fontWeight: AppFontWeight.regular);
+  static TextStyle f18w500 = _textStyle.copyWith(fontSize: AppFontSize.lg, fontWeight: AppFontWeight.medium);
+  static TextStyle f18w600 = _textStyle.copyWith(fontSize: AppFontSize.lg, fontWeight: AppFontWeight.semibold);
+  static TextStyle f18w700 = _textStyle.copyWith(fontSize: AppFontSize.lg, fontWeight: AppFontWeight.bold);
 
-  static TextStyle titleSmall = _textStyle.copyWith(fontSize: AppFontSize.xs, fontWeight: AppFontWeight.semibold, height: _height(AppLineHeight.xs, AppFontSize.xs));
+  static TextStyle f20w400 = _textStyle.copyWith(fontSize: AppFontSize.xl, fontWeight: AppFontWeight.regular);
+  static TextStyle f20w500 = _textStyle.copyWith(fontSize: AppFontSize.xl, fontWeight: AppFontWeight.medium);
+  static TextStyle f20w600 = _textStyle.copyWith(fontSize: AppFontSize.xl, fontWeight: AppFontWeight.semibold);
+  static TextStyle f20w700 = _textStyle.copyWith(fontSize: AppFontSize.xl, fontWeight: AppFontWeight.bold);
 
-  static TextStyle bodyLarge = _textStyle.copyWith(fontSize: AppFontSize.md, fontWeight: AppFontWeight.regular, height: _height(AppLineHeight.md, AppFontSize.md));
-
-  static TextStyle bodyLargeMedium = _textStyle.copyWith(fontSize: AppFontSize.md, fontWeight: AppFontWeight.medium, height: _height(AppLineHeight.md, AppFontSize.md));
-
-  static TextStyle bodyLargeSemiBold = _textStyle.copyWith(fontSize: AppFontSize.md, fontWeight: AppFontWeight.semibold, height: _height(AppLineHeight.md, AppFontSize.md));
-
-  static TextStyle bodyMedium = _textStyle.copyWith(fontSize: AppFontSize.sm, fontWeight: AppFontWeight.regular, height: _height(AppLineHeight.sm, AppFontSize.sm));
-
-  static TextStyle bodyMediumMed = _textStyle.copyWith(fontSize: AppFontSize.sm, fontWeight: AppFontWeight.medium, height: _height(AppLineHeight.sm, AppFontSize.sm));
-
-  static TextStyle bodyMediumSemiBold = _textStyle.copyWith(fontSize: AppFontSize.sm, fontWeight: AppFontWeight.semibold, height: _height(AppLineHeight.sm, AppFontSize.sm));
-
-  static TextStyle bodyCaption = _textStyle.copyWith(fontSize: AppFontSize.xs, fontWeight: AppFontWeight.regular, height: _height(AppLineHeight.xs, AppFontSize.xs));
-
-  static TextStyle bodyCaptionMedium = _textStyle.copyWith(fontSize: AppFontSize.xs, fontWeight: AppFontWeight.medium, height: _height(AppLineHeight.xs, AppFontSize.xs));
-
-  static TextStyle bodyCaptionSemiBold = _textStyle.copyWith(fontSize: AppFontSize.xs, fontWeight: AppFontWeight.semibold, height: _height(AppLineHeight.xs, AppFontSize.xs));
-
-  static TextStyle bodySmall = _textStyle.copyWith(fontSize: AppFontSize.xxs, fontWeight: AppFontWeight.regular, height: _height(AppLineHeight.xxs, AppFontSize.xxs));
-
-  static TextStyle bodySmallMedium = _textStyle.copyWith(fontSize: AppFontSize.xxs, fontWeight: AppFontWeight.medium, height: _height(AppLineHeight.xxs, AppFontSize.xxs));
-
-  static TextStyle bodySmallSemiBold = _textStyle.copyWith(fontSize: AppFontSize.xxs, fontWeight: AppFontWeight.semibold, height: _height(AppLineHeight.xxs, AppFontSize.xxs));
+  static TextStyle f24w400 = _textStyle.copyWith(fontSize: AppFontSize.xxl, fontWeight: AppFontWeight.regular);
+  static TextStyle f24w500 = _textStyle.copyWith(fontSize: AppFontSize.xxl, fontWeight: AppFontWeight.medium);
+  static TextStyle f24w600 = _textStyle.copyWith(fontSize: AppFontSize.xxl, fontWeight: AppFontWeight.semibold);
+  static TextStyle f24w700 = _textStyle.copyWith(fontSize: AppFontSize.xxl, fontWeight: AppFontWeight.bold);
 }
